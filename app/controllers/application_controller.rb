507b91a9efd7de8190ee14c4616c588577dc144e
Caroline Powell <carpowell10@gmail.com>
2017-02-07 23:31:41 -0700

    Add admin login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_admin
+
+  def current_admin
+    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
+  end
 end
---
+++ spec/support/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :admin do
+    name "Example Admin"
+    username "ExampleAdmin3000"
+    email "admin@example.com"
+    password "boom"
+  end
+
+end
