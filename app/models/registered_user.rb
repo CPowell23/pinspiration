6dae8ea9843d2d398e78ef8cce664086de3b05bb
Molly Brown <mollbrown@gmail.com>
2017-02-08 15:25:03 -0700

    add seed file

--- app/models/registered_user.rb
+++ app/models/registered_user.rb
@@ -1,6 +1,9 @@
 class RegisteredUser < ApplicationRecord
   validates :status, presence: true
 
-  has_one :pinspiration_credential
-  has_one :google_credential
+  has_many :pinspiration_credentials
+  has_many :google_credentials
+  has_many :boards
+
+  enum status: [:offline, :online]
 end
