451cd524f4e4d45a5e38200ac06fc871740444e9
Ann Wolff <annwolffe@gmail.com>
2017-02-07 18:48:38 -0700

    Initial sketch for

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,32 @@ Rails.application.routes.draw do
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     get '/dashboard', to: "dashboard#index"
+#    get '/logout', to: 'sessions#destroy'
+#    resources :pins, only: [:destroy]
+#    resources :boards, only: [:destroy]
+#    resources :comments, only: [:destroy]
+#    resources :registered_users, only: [:update]
   end
+
+#  namespace :registered_users, path: ":username" do
+#    resources :boards, only: [:index, :new, :create]
+#    get '/:name', to: 'boards#show'
+#    get '/:name/edit', to: 'boards#edit'
+#    patch '/:name', to: 'boards#update'
+#    delete '/:name', to: 'boards#destroy'
+#    post '/:name/like', to: 'like#create'
+#  end
+  
+#  resources :pins, except: [:index] do
+#    post '/comments', to: 'comments#create'
+#    post '/comments/like', to: 'like#create'
+#    post '/like', to: 'like#create'
+#  end
+
+#  post '/login', to: 'sessons#create'
+#  delete '/logout', to: 'sessions#destroy'
+
+#  get '/:username', to: 'registered_users#show'
+#  get '/:username/pins', to: 'pins#index'
+#
 end
