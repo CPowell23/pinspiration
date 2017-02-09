cd26873ba698b6a01be9a896fdb307877d47bfff
Caroline Powell <carpowell10@gmail.com>
2017-02-08 21:53:40 -0700

    Add pins index

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,17 @@
+class PinsController < ApplicationController
+  def index
+    @user = find_user_by_username(params[:username])
+    @pins = @user.pins
+  end
+
+  private
+
+    def find_user_by_username(username)
+      if GoogleCredential.find_by(username: username)
+        credentials = GoogleCredential.find_by(username)
+      else
+        credentials = PinspirationCredential.find_by(username: username)
+      end
+      credentials.registered_user
+    end
+end
--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -7,4 +7,48 @@ FactoryGirl.define do
     password "boom"
   end
 
+  factory :pin do
+    description Faker::Hipster.sentence
+    image_url "https://s-media-cache-ak0.pinimg.com/564x/90/5d/a9/905da92511ef7e18b65e5c62cf1db38c.jpg"
+    article_url "http://www.ravelry.com/patterns/library/black-roxy"
+    board
+  end
+
+  factory :board do
+    name Faker::Hipster.word
+    description Faker::Hipster.sentence
+    private false
+    category
+    registered_user
+  end
+
+  factory :category do
+    sequence :name do |n|
+      "#{n} Category"
+    end
+  end
+
+  factory :registered_user do
+    status "online"
+
+    after(:create) do |registered_user|
+      create(:pinspiration_credential, registered_user: registered_user)
+    end
+  end
+
+  factory :pinspiration_credential do
+    sequence :name do |n|
+      "#{n} Name"
+    end
+    sequence :username do |n|
+      "#{n}username"
+    end
+    sequence :email do |n|
+      "#{n}@fake.com"
+    end
+    password "pasword"
+    phone_number "555-555-5555"
+    registered_user
+  end
+
 end
