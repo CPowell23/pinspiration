60cebeb7166ddb3f038fee75922d4f185110142c
Ann Wolff <annwolffe@gmail.com>
2017-02-09 15:48:14 -0700

    Adjust pins routes for namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -12,26 +12,26 @@ Rails.application.routes.draw do
 #    resources :registered_users, only: [:update]
   end
 
-namespace :registered_users, path: ":username" do
-  resources :boards, only: [:index]  
-  get "/:name", to: "boards#show", as: "board"
+  resources :pins, except: [:index]  do
+  #  post '/comments', to: 'comments#create'
+  #  post '/comments/like', to: 'like#create'
+  #  post '/like', to: 'like#create'
+  end
+  
+  namespace :registered_users, path: ":username" do
+    resources :boards, only: [:index]  
+    resources :pins, only: [:index]
+    get "/:name", to: "boards#show", as: "board"
 #    resources :boards, only: [:index, :new, :create]
 #    get '/:name/edit', to: 'boards#edit'
 #    patch '/:name', to: 'boards#update'
 #    delete '/:name', to: 'boards#destroy'
 #    post '/:name/like', to: 'like#create'
   end
-
-resources :pins, except: [:index] do
-  #  post '/comments', to: 'comments#create'
-  #  post '/comments/like', to: 'like#create'
-  #  post '/like', to: 'like#create'
- end
-  
+ 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   get '/:username', to: 'registered_users#show'
-  get '/:username/pins', to: 'pins#index', as: 'pins_index'
 
 end
--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -3,7 +3,7 @@ module TestHelpers
     page.set_rack_session(admin_id: admin.id)
   end
 
-  def stub_log_in_registered_user(user)
+  def stub_log_in_user(user)
     page.set_rack_session(registered_user_id: user.id)
   end
 end
