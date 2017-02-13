a8e3d41748d6d9044946c4a0b1b3cae4023086c6
Ann Wolff <annwolffe@gmail.com>
2017-02-13 16:32:32 -0700

    Add masonry to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,11 @@ class HomeController < ApplicationController
   before_action :require_login
 
   def index
-    if current_user.followers.count != 0
+    if current_user.followings.count != 0
       user = RegisteredUser.find(current_user.id)
       @activity = Pin.joins(registered_user: :followers).where(:follow_joins => {follower_id: user.id}).sort_by_most_recent
     else
-      @activity = Pin.order("RAND()").first(20)
+      @activity = Pin.order("RANDOM()").first(20)
     end
   end
 end
