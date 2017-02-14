c339dc61314d3aa5eeb7972ebd4d2a2a252688aa
Ann Wolff <annwolffe@gmail.com>
2017-02-14 09:14:16 -0700

    Authenticate user edit page

--- spec/features/users/user_deactivate_account_spec.rb
+++ spec/features/users/user_deactivate_account_spec.rb
@@ -2,16 +2,23 @@ require 'rails_helper'
 
 describe 'User deactives account' do
   before do
-    @user = create(:registered_user)
-    stub_log_in_user(@user)
+    @user1 = create(:registered_user)
+    @user2 = create(:registered_user)
+    stub_log_in_user(@user2)
   end
 
   it 'from edit path' do
-    visit edit_registered_user_path(@user.username)
+    visit edit_registered_user_path(@user2.username)
     click_on "Deactivate Account"
-    @user.reload
+    @user2.reload
 
     expect(current_path).to eq('/login')
-    expect(@user.status).to eq('offline')
+    expect(@user2.status).to eq('offline')
+  end
+
+  it 'cannot deactivate another users account' do
+    visit edit_registered_user_path(@user1.username)
+
+    expect(page).to have_content('404')
   end
 end
