7dc20091b003aad6eb58e1f683cab0416fb25702
Ann Wolff <annwolffe@gmail.com>
2017-02-13 17:07:39 -0700

    Adjust line height and color

--- spec/features/users/user_sees_account_spec.rb
+++ spec/features/users/user_sees_account_spec.rb
@@ -4,12 +4,10 @@ describe 'User can view account information' do
   before do
     @user = create(:registered_user)
     stub_log_in_user(@user)
-    visit registered_user_path(@user) 
+    visit registered_user_path(@user.username) 
   end
 
   it 'by using account icon' do
-    expect(current_path).to eq("/#{@user.username}")
-    expect(page).to have_content("Account Profile")
     expect(page).to have_content(@user.name)
     expect(page).to have_content("Boards")
     expect(page).to have_content("Pins")
