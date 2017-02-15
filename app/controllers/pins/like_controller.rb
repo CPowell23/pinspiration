0a96775d36938208db19ce4b60872150c09ddd44
Ann Wolff <annwolffe@gmail.com>
2017-02-15 16:50:37 -0700

    Add like button to pin card

--- app/controllers/pins/like_controller.rb
+++ app/controllers/pins/like_controller.rb
@@ -3,14 +3,14 @@ class Pins::LikeController < ApplicationController
   def create
     pin = Pin.find(params[:pin_id])
     Like.create(target: pin, registered_user: current_user) 
-    redirect_to pin_path(pin)
+    redirect_back fallback_location: pin_path(pin)
   end
   
   def destroy
     pin = Pin.find(params[:pin_id])
     like = Like.find(params[:id])
     like.destroy
-    redirect_to pin_path(pin)
+    redirect_back fallback_location: pin_path(pin)
   end
 
 end
