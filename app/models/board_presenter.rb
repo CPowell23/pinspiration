9f18a84f8198eaf461487530029f28f9f01f3080
Ann Wolff <annwolffe@gmail.com>
2017-02-16 07:16:31 -0700

    Fix board presenter for Google Credentials

--- app/models/board_presenter.rb
+++ app/models/board_presenter.rb
@@ -2,9 +2,17 @@ class BoardPresenter
   attr_reader :user, :board, :pins
 
   def initialize(username, board_name)
-    @user = PinspirationCredential.find_by(username: username).registered_user
+    @user = find_user(username)
     @board = @user.boards.find_by(name: board_name)
     @pins = @board.pins
   end
 
+  def find_user(username)
+    credentials = PinspirationCredential.find_by(username: username)
+    if credentials 
+      @user = creditals.registered_user
+    else
+      @user = GoogleCredential.find_by(username: username).registered_user
+    end
+  end
 end
